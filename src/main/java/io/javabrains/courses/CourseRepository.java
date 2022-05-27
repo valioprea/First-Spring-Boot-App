44d10950487109524256472e0479fb2cc675fe57
valioprea <valentin.oprea@ferendia.com>
2022-05-27 12:16:51 +0300

    First Commit

---
+++ src/main/java/io/javabrains/courses/CourseRepository.java
@@ -0,0 +1,10 @@
+package io.javabrains.courses;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+
+    public List<Course> findByTopicId(String topicId);
+}
